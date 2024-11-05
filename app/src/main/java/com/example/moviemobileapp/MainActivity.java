d364dd6fe1fd35b76a856ef1c68c628168f86c6d
maxli97 <maxli19970830@gmail.com>
2024-11-04 16:29:02 -0800

    Latest right version for max

---
+++ app/src/main/java/com/example/moviemobileapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.moviemobileapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.btnSignUp);
+        name=findViewById(R.id.txtEmail);
+        System.out.println(123);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "2333333", Toast.LENGTH_SHORT).show();
+                name.setText("123125412");
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
